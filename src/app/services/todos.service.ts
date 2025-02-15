f920131373dcf58e068490c1d4c868fbdee53423
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2025-02-15 15:22:29 +0530

    Making HTTP calls inAngular Services

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,20 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-  todoItems: Array<Todo> = [{
-    userId : 1,
-    completed: false,
-    title: 'groceries',
-    id: 0
-  },{
-    userId : 1,
-    completed: false,
-    title: 'car wash',
-    id: 1
-  }]
-  constructor() { }
+
+  http = inject(HttpClient);
+
+  getTodosFromApi(){
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    return this.http.get<Array<Todo>>(url);
+  }
 }
