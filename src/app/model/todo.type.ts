37b168cbac9c5e36fa9fde22badec52005986af9
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2025-02-15 14:45:08 +0530

    using injetables in angular

---
+++ src/app/model/todo.type.ts
@@ -0,0 +1,6 @@
+export type Todo = {
+    userId : Number;
+    completed: boolean;
+    title: string;
+    id: number;
+}
---
+++ src/app/services/todos.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosService {
+  todoItems: Array<Todo> = [{
+    userId : 1,
+    completed: false,
+    title: 'groceries',
+    id: 0
+  },{
+    userId : 1,
+    completed: false,
+    title: 'car wash',
+    id: 1
+  }]
+  constructor() { }
+}
